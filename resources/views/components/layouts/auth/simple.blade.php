40084c400319bc7ae0d7a888fa14a38349f4fb74
fsay2604 <hebert.francois.charles@gmail.com>
2026-01-08 19:09:41 -0500

    attempt to fix login heigh using dvh

--- resources/views/components/layouts/auth/simple.blade.php
+++ resources/views/components/layouts/auth/simple.blade.php
@@ -3,9 +3,9 @@
     <head>
         @include('partials.head')
     </head>
-    <body class="min-h-screen bg-white antialiased dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900">
+    <body class="min-h-screen antialiased dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900">
         <div style="background-image: url('{{ asset('storage/images/background.png') }}');"
-            class="bg-background flex min-h-svh flex-col items-center justify-center gap-6 bg-cover bg-center bg-no-repeat p-4 md:p-8 md:pt-0"
+            class="flex min-h-[100dvh] w-full flex-col items-center justify-center gap-6 bg-cover bg-center bg-no-repeat p-4 md:p-8 md:pt-0"
         >
             <div class="flex w-full max-w-sm flex-col gap-2">
                 <a href="{{ route('home') }}" class="flex w-full flex-col items-center gap-2 font-medium" wire:navigate>

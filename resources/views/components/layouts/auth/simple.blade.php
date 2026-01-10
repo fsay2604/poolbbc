a3f6efe9406511a6b6037e700e4e46f6feb898ac
fsay2604 <hebert.francois.charles@gmail.com>
2026-01-10 10:33:58 -0500

    Attempt to fix auth background.

--- resources/views/components/layouts/auth/simple.blade.php
+++ resources/views/components/layouts/auth/simple.blade.php
@@ -3,10 +3,7 @@
     <head>
         @include('partials.head')
     </head>
-    <body class="min-h-screen antialiased dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900">
-        <div style="background-image: url('{{ asset('storage/images/background.png') }}');"
-            class="flex min-h-[100dvh] w-full flex-col items-center justify-center gap-6 bg-cover bg-center bg-no-repeat p-4 md:p-8 md:pt-0"
-        >
+    <body class="min-h-screen antialiased dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900 bg-[url('{{ asset('storage/images/background.png') }}')] bg-cover bg-center bg-no-repeat">
             <div class="flex w-full max-w-sm flex-col gap-2">
                 <a href="{{ route('home') }}" class="flex w-full flex-col items-center gap-2 font-medium" wire:navigate>
                     <span class="flex w-full items-center justify-center">
@@ -22,7 +19,6 @@
                     {{ $slot }}
                 </div>
             </div>
-        </div>
         @fluxScripts
     </body>
 </html>
